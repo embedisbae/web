d05f2a4b44d8162bb4ddc8284c5cd677c5153b44
Daniel <daniel@lngzl.nl>
2019-10-28 22:27:05 +0100

    :art: Container

--- src/routes/partners.svelte
+++ src/routes/partners.svelte
@@ -15,16 +15,18 @@
 	}
 </style>
 
-<h1 class="title">We have some partners!</h1>
+<div class="container text-justify">
+	<h1 class="title">We have some partners!</h1>
 
-<p><b><a href="https://file.coffee">File.coffee</a></b>
-Love to Share files that are larger than 8MB via Discord or need it for a project? Or make a lot of screenshots but also love privacy? Then try out file.coffee, it's the anonymous file sharing service. No accounts, no registration, just "plug and play".
-</p>
+	<p><b><a href="https://file.coffee">File.coffee</a></b>
+	Love to Share files that are larger than 8MB via Discord or need it for a project? Or make a lot of screenshots but also love privacy? Then try out file.coffee, it's the anonymous file sharing service. No accounts, no registration, just "plug and play".
+	</p>
 
-<p><b><a href="https://points.city">Points</a></b>
-Points is a clean yet very customizable points bot that features an simple and modern dashboard. Points adds one of the most intuitive yet customizable economy style experiences to any server.
-</p>
+	<p><b><a href="https://points.city">Points</a></b>
+	Points is a clean yet very customizable points bot that features an simple and modern dashboard. Points adds one of the most intuitive yet customizable economy style experiences to any server.
+	</p>
 
-<p><b><a href="https://crystalbotlist.uk">CrystalBotList.uk</a></b>
-Crystal Bot List is a growing directory of Discord bots to enhance your server with bot creators adding their newly designed bot constantly, this allows users from all over the world to browse and add the desired bot to their own server with a click of a button without any hassle or 'know how'.
-</p>
+	<p><b><a href="https://crystalbotlist.uk">CrystalBotList.uk</a></b>
+	Crystal Bot List is a growing directory of Discord bots to enhance your server with bot creators adding their newly designed bot constantly, this allows users from all over the world to browse and add the desired bot to their own server with a click of a button without any hassle or 'know how'.
+	</p>
+</div>
