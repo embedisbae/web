63887d82c3c9988042f44a701da272cda3aa87b1
Daniel <daniel@lngzl.nl>
2019-10-28 22:27:14 +0100

    :art: Container

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -28,14 +28,15 @@
 	<title>Blog</title>
 </svelte:head>
 
-<h1 class="title">Let's start reading!</h1>
-
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
-	{/each}
-</ul>
+<div class="container">
+	<h1 class="title">Let's start reading!</h1>
+	<ul>
+		{#each posts as post}
+			<!-- we're using the non-standard `rel=prefetch` attribute to
+					tell Sapper to load the data for the page as soon as
+					the user hovers over the link or taps it, instead of
+					waiting for the 'click' event -->
+			<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
+		{/each}
+	</ul>
+</div>
