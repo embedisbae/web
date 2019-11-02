5aa6724ebedfab8ec67744456316466385a1bb34
Daniel <daniel@lngzl.nl>
2019-11-02 15:13:38 +0100

    :sparkles: Fix

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
 
 express() // You can also use Express
     .use(
@@ -15,4 +14,5 @@ express() // You can also use Express
     )
     .listen(PORT, err => {
         if (err) console.log('error', err);
+        console.log('ENV: ' + NODE_ENV);
     });
