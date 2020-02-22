3a155852707149dc1c71c9c730ddee023aa989ac
embed <brydon@bubblez.xyz>
2020-02-22 05:00:27 -0500

    Update partners.svelte

--- src/routes/partners.svelte
+++ src/routes/partners.svelte
@@ -9,7 +9,7 @@
     <div class="card border-0 shadow-lg mb-3">
       <div class="card-body">
         <div class="media">
-          <a href="https://file.coffee"><img alt="file.coffee Logo" src="https://file.coffee/QGUrNODuz.png" class="mr-3 w-10 rounded" draggable="false"></a>
+          <a href="https://file.coffee"><img alt="file.coffee Logo" src="https://file.coffee/QGUrNODuz.png" class="mr-3 w-10 rounded img-p" draggable="false"></a>
           <div class="media-body">
             <h5 class="card-title"><b><a href="https://file.coffee">file.coffee</a></b></h5>
             <p class="card-text">Love to Share files that are larger than 8MB via Discord or need it for a project? Or make a lot of screenshots but also love privacy? Then try out file.coffee, it's the anonymous file sharing service. No accounts, no registration, just "plug and play".</p>
@@ -20,7 +20,7 @@
     <div class="card border-0 shadow-lg mb-3">
       <div class="card-body">
         <div class="media">
-          <a href="https://points.city"><img alt="Points Logo" src="https://points.city/images/logo/Discord.png" class="mr-3 w-10 rounded" draggable="false"></a>
+          <a href="https://points.city"><img alt="Points Logo" src="https://points.city/images/logo/Discord.png" class="mr-3 w-10 rounded img-p" draggable="false"></a>
           <div class="media-body">
             <h5 class="card-title"><b><a href="https://points.city">Points</a></b></h5>
             <p class="card-text">Points is a clean yet very customizable points bot that features an simple and modern dashboard. Points adds one of the most intuitive yet customizable economy style experiences to any server.</p>
@@ -31,7 +31,7 @@
     <div class="card border-0 shadow-lg">
       <div class="card-body">
         <div class="media">
-          <a href="https://crystalbotlist.uk"><img alt="CrystalBotList Logo" src="https://file.coffee/OZ5vZd6xF.png" class="mr-3 w-10 rounded" draggable="false"></a>
+          <a href="https://crystalbotlist.uk"><img alt="CrystalBotList Logo" src="https://file.coffee/OZ5vZd6xF.png" class="mr-3 w-10 rounded img-p" draggable="false"></a>
           <div class="media-body">
             <h5 class="card-title"><b><a href="https://crystalbotlist.uk">Crystal Bot List</a></b></h5>
             <p class="card-text">Crystal Bot List is a growing directory of Discord bots to enhance your server with bot creators adding their newly designed bot constantly, this allows users from all over the world to browse and add the desired bot to their own server with a click of a button without any hassle or 'know how'.</p>
